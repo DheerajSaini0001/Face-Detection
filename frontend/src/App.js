940e5a2aea56f23aa6e8781a6a261b02a4d5f89d
Dheeraj Saini <dheerajsaini131652@gmail.com>
2025-12-05 13:17:02 +0530

    Done for the frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import * as faceapi from 'face-api.js';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import FeaturesPage from './pages/FeaturesPage';
+import PricingPage from './pages/PricingPage';
+import AboutPage from './pages/AboutPage';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import TermsOfService from './pages/TermsOfService';
+import ContactPage from './pages/ContactPage';
+import './App.css';
+
+function App() {
+  const videoRef = useRef(null);
+  const [modelsLoaded, setModelsLoaded] = useState(false);
+  const [detections, setDetections] = useState([]);
+
+  useEffect(() => {
+    // Load face-api.js models
+    const loadModels = async () => {
+      const MODEL_URL = process.env.PUBLIC_URL + '/models';
+      await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+      await faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL);
+      await faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL);
+      await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
+      setModelsLoaded(true);
+    };
+
+    loadModels();
+  }, []);
+
+  const handleVideoOnPlay = () => {
+    setInterval(async () => {
+      if (videoRef.current) {
+        const detections = await faceapi.detectAllFaces(
+          videoRef.current,
+          new faceapi.TinyFaceDetectorOptions()
+        ).withFaceLandmarks().withFaceExpressions();
+
+        setDetections(detections);
+      }
+    }, 100);
+  };
+
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Home
+                  modelsLoaded={modelsLoaded}
+                  videoRef={videoRef}
+                  handleVideoOnPlay={handleVideoOnPlay}
+                  detections={detections}
+                />
+              }
+            />
+            <Route path="/features" element={<FeaturesPage />} />
+            <Route path="/pricing" element={<PricingPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/privacy" element={<PrivacyPolicy />} />
+            <Route path="/terms" element={<TermsOfService />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/FeaturesPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Features from '../components/Features';
+
+const FeaturesPage = () => {
+    return (
+        <div className="features-page">
+            <Features />
+        </div>
+    );
+};
+
+export default FeaturesPage;
